7ab1aafe5ce151abaf42649ac84270b41347fcf8
[yunn3] <[rjafe@uncc.edu]>
2022-10-28 12:17:41 -0400

    sigin comp

--- src/app/signin/signin.component.ts
+++ src/app/signin/signin.component.ts
@@ -10,6 +10,96 @@ export class SigninComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+
+    const container = document.querySelector(".container"),
+      pwShowHide = document.querySelectorAll(".showHidePw"),
+      pwFields: NodeListOf<HTMLInputElement> = document.querySelectorAll(".password"),
+      signUp = document.querySelector(".signup-link"),
+      login = document.querySelector(".login-link");
+
+    pwShowHide.forEach(eyeIcon => {
+      eyeIcon.addEventListener("click", () => {
+
+        //   js code to show/hide password and change icon
+        pwFields.forEach(pwField => {
+          if (pwField.type == "password") {
+            pwField.type = "text";
+
+            pwShowHide.forEach(icon => {
+              icon.classList.replace("uil-eye-slash", "uil-eye");
+            })
+          } else {
+            pwField.type = "password";
+
+            pwShowHide.forEach(icon => {
+              icon.classList.replace("uil-eye", "uil-eye-slash");
+            })
+          }
+        })
+      })
+    })
+    // js code to check match password and confirm password
+    var check = function () {
+      const input = document.getElementById("password") as HTMLInputElement;
+      const input2 = document.getElementById("confirm_password") as HTMLInputElement;
+      if (input?.value == "" && input2?.value == "") {
+        document!.getElementById("message")!.style.color = "red";
+        document!.getElementById("message")!.innerHTML = "Please Enter password atleast 6 characters";
+        const btn = document!.getElementById("submit_button") as HTMLButtonElement;
+        btn!.disabled = true;
+      } else {
+        if (input.value == input2.value) {
+          document!.getElementById("message")!.style.color = "green";
+          document!.getElementById("message")!.innerHTML = "password matched";
+          const btn = document!.getElementById("submit_button") as HTMLButtonElement;
+
+          btn.disabled = false;
+        } else {
+          document!.getElementById("message")!.style.color = "red";
+          document!.getElementById("message")!.innerHTML = "password not matching";
+        }
+      }
+    };
+
+    //login info check
+    var check_info = function () {
+      const input = document.getElementById("username") as HTMLInputElement;
+      const input2 = document.getElementById("userpassword") as HTMLInputElement;
+      if (input.value == "" || input2.value == "") {
+        var element = document.getElementById("login_msg");
+
+        document!.getElementById("message_container")!.style.display = "block";
+        element?.classList.remove("success");
+        element?.classList.add("error");
+        element!.innerHTML = "Invalide Username and password";
+
+        input.style.marginTop = "10px";
+      } else {
+        var element = document.getElementById("login_msg");
+        document!.getElementById("message_container")!.style.display = "block";
+        element?.classList.remove("error");
+        element?.classList.add("success");
+        element!.innerHTML = "login successfully done";
+
+        input.style.marginTop = "10px";
+      }
+    };
+
+    // js code to appear signup and login form
+    signUp?.addEventListener("click", () => {
+    container?.classList.add("active");
+    });
+    login?.addEventListener("click", () => {
+      container?.classList.remove("active");
+    });
+
+
   }
 
+
 }
+
+
+
+
+
