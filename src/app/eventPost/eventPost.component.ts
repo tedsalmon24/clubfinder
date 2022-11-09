abcae61ed67cd2a6f7f9d95b91feb7b6378653b4
Taylor Rogers <troger43@uncc.edu>
2022-11-09 11:27:41 -0500

    added EventPost Page

---
+++ src/app/eventPost/eventPost.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-settings',
+  templateUrl: './eventPost.component.html',
+  styleUrls: ['./eventPost.component.css']
+})
+export class eventPostComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
